ad2e98f0f954f206e1c4c6158ac2ddde1a44c1d2
azzipmp <azzipmp@gmail.com>
2019-09-08 12:37:17 -0400

    updated home page

--- CucumberPaginationFramework2/src/test/java/Pages/HomePage.java
+++ CucumberPaginationFramework2/src/test/java/Pages/HomePage.java
@@ -19,6 +19,8 @@ public class HomePage {
 	
 	@FindBy(name="userName")
 	WebElement usernametextfield;
+	@FindBy(name="userName1")
+	WebElement usernametextfield1;
 	
 	public HomePage(WebDriver driver)
 	{
